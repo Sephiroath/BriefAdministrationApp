d9517dce065fc824c2ab27ed3e9b961e933413bc
caalbarraicnm@gmail.com <Andres1230>
2018-03-30 13:23:32 -0500

    changes in services

--- src/app/projectmanagement/projectconfiguration/GcrmStatus/gcrmstatus.reducer.ts
+++ src/app/projectmanagement/projectconfiguration/GcrmStatus/gcrmstatus.reducer.ts
@@ -1,6 +1,7 @@
 import { v4 as uuid } from 'uuid';
 import { Action } from '@ngrx/store';
 import { HttpErrorResponse } from '@angular/common/http';
+import { GcrmState } from '../Models/ProjectConfigurationModels';
 
 export const GCRMSTATUS_KEY = 'PROJECTMANAGEMENT.GCRMSTATUS';
 
@@ -12,17 +13,17 @@ export enum GcrmStatusActionTypes {
 
 export class ActionGcrmStatusGet implements Action {
     readonly type = GcrmStatusActionTypes.GETGCRMSTATUSES;
-    constructor(public payload: { }) { }
+    constructor(public payload: {}) { }
 }
 
 export class ActionGcrmStatusGetSuccess implements Action {
-    readonly type = GcrmStatusActionTypes.GETGCRMSTATUSES;
-    constructor(public payload: { }) { }
+    readonly type = GcrmStatusActionTypes.GETGCRMSTATUSESUCCESS;
+    constructor(public payload: { gcrmstatuses: GcrmState[] }) { }
 }
 
 export class ActionGcrmStatusGetError implements Action {
-    readonly type = GcrmStatusActionTypes.GETGCRMSTATUSES;
-    constructor(public payload: { }) { }
+    readonly type = GcrmStatusActionTypes.GETGCRMSTATUSESERROR;
+    constructor(public payload: { error: HttpErrorResponse }) { }
 }
 
 export type GcrmStatesActions =
@@ -30,8 +31,32 @@ export type GcrmStatesActions =
     | ActionGcrmStatusGetSuccess
     | ActionGcrmStatusGetError;
 
-export function GcrmStateReducer(){
-    
+export function GcrmStateReducer(
+    state: GcrmStatusState = initialState,
+    action: GcrmStatesActions): GcrmStatusState {
+    switch (action.type) {
+        case GcrmStatusActionTypes.GETGCRMSTATUSES:
+            return {
+                ...state,
+                loading: true,
+                error: null,
+                gcrmStates: null
+            }
+        case GcrmStatusActionTypes.GETGCRMSTATUSESUCCESS:
+            return {
+                ...state,
+                loading: true,
+                error: null,
+                gcrmStates: action.payload.gcrmstatuses
+            }
+        case GcrmStatusActionTypes.GETGCRMSTATUSESERROR:
+            return {
+                ...state,
+                loading: true,
+                error: action.payload.error,
+                gcrmStates: null
+            }
+    }
 }
 
 export const initialState: GcrmStatusState = {
@@ -49,4 +74,5 @@ export interface GcrmState {
 export interface GcrmStatusState {
     loading: boolean,
     gcrmStates?: GcrmState[]
+    error?: HttpErrorResponse
 }
